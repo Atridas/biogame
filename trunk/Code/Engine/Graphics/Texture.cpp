89e2b42fbf99082f570134c8e84215556a93c335
Atridas87@gmail.com <Atridas87@gmail.com@576ee6d0-068d-96d9-bff2-16229cd70485>
2010-11-24 20:28:24 +0000

    Implementat la CTexture i afegida CTextureManager.

--- trunk/Code/Engine/Graphics/Texture.cpp
+++ trunk/Code/Engine/Graphics/Texture.cpp
@@ -1,33 +1,72 @@
-#include "include\Texture.h"
+#include "Texture.h"
+
+#include "RenderManager.h"
+#include "Core.h"
 
 
 bool CTexture::LoadFile() 
 {
-  LOGGER->AddNewLog(ELL_INFORMATION, "CTexture::Load \"%s\"",m_FileName.c_str());
-  return true;
+  LOGGER->AddNewLog(ELL_INFORMATION, "CTexture::Load \"%s\"",m_szFileName.c_str());
+
+  LPDIRECT3DDEVICE9 l_pDevice = RENDER_MANAGER->GetDevice();
+
+  HRESULT l_Result = D3DXCreateTextureFromFile( l_pDevice, m_szFileName.c_str(), &m_pTexture);
+
+  if(l_Result == D3D_OK )
+    return true;
+
+  //TODO fer logs més millors
+
+  if(l_Result == D3DERR_NOTAVAILABLE) {
+    LOGGER->AddNewLog(ELL_ERROR, "CTexture::Load D3DERR_NOTAVAILABLE");
+  } else if(l_Result == D3DERR_OUTOFVIDEOMEMORY) {
+    LOGGER->AddNewLog(ELL_ERROR, "CTexture::Load D3DERR_OUTOFVIDEOMEMORY");
+  } else if(l_Result == D3DERR_NOTAVAILABLE) {
+    LOGGER->AddNewLog(ELL_ERROR, "CTexture::Load D3DERR_INVALIDCALL");
+  } else if(l_Result == D3DERR_NOTAVAILABLE) {
+    LOGGER->AddNewLog(ELL_ERROR, "CTexture::Load D3DERR_INVALIDDATA");
+  } else if(l_Result == E_OUTOFMEMORY) {
+    LOGGER->AddNewLog(ELL_ERROR, "CTexture::Load E_OUTOFMEMORY");
+  }
+
+  return false;
 }
 
 void CTexture::Unload()
 {
    LOGGER->AddNewLog(ELL_INFORMATION, "CTexture::Unload");
+
+   CHECKED_RELEASE(m_pTexture)
 }
 
-bool CTexture::Load(const std::string &FileName)
+bool CTexture::Load(const std::string &_szFileName)
 {
-  if(m_FileName != "")
+  if(m_szFileName != "")
   {
-    LOGGER->AddNewLog(ELL_WARNING, "CTexture::Load carregant \"%s\" quan ja hi ha \"%s\" carregada",FileName.c_str(),m_FileName.c_str());
+    LOGGER->AddNewLog(ELL_WARNING, "CTexture::Load carregant \"%s\" quan ja hi ha \"%s\" carregada",_szFileName.c_str(),m_szFileName.c_str());
+    Unload();
   }
-  m_FileName = FileName;
+  m_szFileName = _szFileName;
   return LoadFile();
 }
 
 bool CTexture::Reload()
 {
-  return true;
+  Unload();
+  return LoadFile();
 }
 
-void CTexture::Activate(size_t StageId)
+void CTexture::Activate(size_t _StageId)
 {
+  LPDIRECT3DDEVICE9 l_pDevice = RENDER_MANAGER->GetDevice();
 
+  HRESULT l_Result = l_pDevice->SetTexture(_StageId,m_pTexture);
+
+  if(l_Result == D3D_OK)
+    return;
+  if(l_Result == D3DERR_INVALIDCALL) 
+  {
+    LOGGER->AddNewLog(ELL_ERROR, "CTexture::Activate D3DERR_INVALIDCALL");
+    //TODO exception?
+  }
 }
