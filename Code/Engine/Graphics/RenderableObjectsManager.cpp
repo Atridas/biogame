aba1832d69bd5c63155f021b24333d0babfafed1
mudarra <mudarra@576ee6d0-068d-96d9-bff2-16229cd70485>
2010-12-18 19:10:09 +0000

    Canvis a RenderableObjectsManager.cpp i Object3D.h
    No es poden crear nous CRenderableObject perquè és una classe abstracta. Així que s'haurà de meditar si hem de dir el tipo de Renderable Object o que.
    
    No està acabat, falten mètodes a fer dins de RenderableObjectsManager.cpp

--- Code/Engine/Graphics/RenderableObjectsManager.cpp
+++ Code/Engine/Graphics/RenderableObjectsManager.cpp
@@ -2,16 +2,19 @@
 #include "RenderManager.h"
 #include <XML/XMLTreeNode.h>
 
-void CRenderableObjectsManager::Update(float ElapsedTime)
+void CRenderableObjectsManager::Update(float _fElapsedTime)
 {
-  
+  for(int i=0; i < m_RenderableObjects.size() ; i++)
+    m_RenderableObjects[i]->Update(_fElapsedTime);
 }
 
 void CRenderableObjectsManager::Render(CRenderManager *RM)
 {
-  
+  for(int i=0; i < m_RenderableObjects.size() ; i++)
+    m_RenderableObjects[i]->Render(RM);
 }
 
+//TODO
 CRenderableObject* CRenderableObjectsManager::AddMeshInstance(
                                                       const string& _szCoreMeshName,
                                                       const string& _szInstanceName,
@@ -22,7 +25,8 @@ CRenderableObject* CRenderableObjectsManager::AddMeshInstance(
 
 void CRenderableObjectsManager::AddResource(const string& _szName, CRenderableObject* _pRenderableObject)
 {
-
+  m_Resources[_szName] = &_pRenderableObject;
+  m_RenderableObjects.push_back(_pRenderableObject);
 }
 
 bool CRenderableObjectsManager::Load(const string& _szFileName)
@@ -42,28 +46,36 @@ bool CRenderableObjectsManager::Load(const string& _szFileName)
   for(int i = 0; i < l_iNumObjects; i++)
   {
     string l_szName;
-    string l_szPath;
+    Vect3f l_vPos;
+    float l_fYaw;
+    float l_fPitch;
+    float l_fRoll;
     CRenderableObject* l_pRenderableObject = 0;
 
     CXMLTreeNode l_XMLObject = l_XMLObjects(i);
     l_szName = l_XMLObject.GetPszProperty("name" ,"");
-    //l_szPath = l_XMLObject.GetPszProperty("path" ,"");
+    l_vPos = l_XMLObject.GetVect3fProperty("position",Vect3f(0.0f));
+    l_fYaw = l_XMLObject.GetFloatProperty("yaw");
+    l_fPitch = l_XMLObject.GetFloatProperty("pitch");
+    l_fRoll = l_XMLObject.GetFloatProperty("roll");
 
     if(!GetResource(l_szName))
     {
-      //l_pRenderableObject = new CRenderableObject();
+      //TODO: uncomment
+      //l_pRenderableObject = new CRenderableObject(l_vPos, l_fYaw, l_fPitch, l_fRoll);
 
       LOGGER->AddNewLog(ELL_INFORMATION,"CRenderableObjectsManager:: Adding object: \"%s\"", l_szName.c_str());
       //AddResource(l_szName,l_pRenderableObject);
 
     }else{
-      LOGGER->AddNewLog(ELL_INFORMATION,"CRenderableObjectsManager:: Repeated object \"%s\"", l_szName.c_str());
+      LOGGER->AddNewLog(ELL_WARNING,"CRenderableObjectsManager:: Repeated object \"%s\"", l_szName.c_str());
     }
   }
 
   return true;
 }
 
+//TODO
 CRenderableObject* CRenderableObjectsManager::GetInstance(const string& _szName) const
 {
   return NULL;
--- Code/Engine/Graphics/include/Object3D.h
+++ Code/Engine/Graphics/include/Object3D.h
@@ -23,18 +23,21 @@ public:
 	float						GetRoll			() const {return m_fRoll;}
 	float						GetPitch		() const {return m_fPitch;}
 	const Vect3f&		GetPosition	() const {return m_Position;}
+  bool						GetVisible	() const {return m_bVisible;}
 
 	//---Set Functions
 	void		SetPosition	( const Vect3f& pos )		{m_Position = pos;}
 	void		SetYaw			( float yaw )						{m_fYaw = yaw;}
 	void		SetPitch		( float pitch )					{m_fPitch = pitch;}
 	void		SetRoll			( float roll )					{m_fRoll = roll;}
+  void		SetVisible	( bool  visible )				{m_bVisible = visible;}
 
 protected:
 	Vect3f	m_Position;
 	float		m_fYaw;
 	float		m_fPitch;
 	float		m_fRoll;
+  bool    m_bVisible;
 };
 
 #endif //INC_CORE_H_
