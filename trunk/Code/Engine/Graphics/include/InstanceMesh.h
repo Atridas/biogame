3259f263a807ea12256211284f09ba90d496a86f
mudarra <mudarra@576ee6d0-068d-96d9-bff2-16229cd70485>
2010-12-19 19:23:41 +0000

    InstanceMesh added. RenderableObjectsManager changes.
    RenderableObjectsManager.cpp s'ha canviat el mètode Render perquè comprovi que l'objecte sigui visible abans de cridar el seu render.

---
+++ trunk/Code/Engine/Graphics/include/InstanceMesh.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#ifndef __MESH_INSTANCE_H__
+#define __MESH_INSTANCE_H__
+
+#include "base.h"
+
+//forward declarations-------------
+class CRenderManager;
+class CStaticMesh;
+//---------------------------------
+
+
+class CInstanceMesh : public CRenderableObject
+{
+public:
+  //Constructor / Destructor
+  CInstanceMesh(const string& _szName, const string& _szCoreName);
+  ~CInstanceMesh()                                                      {CHECKED_DELETE(m_StaticMesh);};
+
+  //Methods
+  void Render(CRenderManager* _pRM);
+
+private:
+  CStaticMesh *m_StaticMesh;
+};
+
+#endif //__MESH_INSTANCE_H__
