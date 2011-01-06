fab984be1ddea49e47ee985f78e4ec1c908191a3
mudarra <mudarra@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-01-06 16:18:52 +0000

    Afegit un mètode a Engine/Graphics/AnimatedModelManager. Documentació Doxygen en estil JavaDoc d'Engine/Base i Engine/Core. S'ha iniciat la documentació de les classes implementades per nosaltres. Mica en mica es seguiran documentant la resta de classes implementades i finalment les classes que ens venien de base. A Docs/Doxyfile.config he deixat un fitxer de configuració pel Doxygen. Per tal de crear la documentació amb doxygen, heu d'obrir aquest fitxer amb doxywizard i només heu d'afegir la ruta base del vostre projecte a Expert/Project/STRIP_FROM_PATH; d'aquesta forma us quedaran els paths macos. La documentació en HTML no la pujo perquè es generen milers de fitxers, un cop ja estigui tota generada busquem un mètode de penjar-la en algun lloc. De moment si la voleu la genereu, sino ja fa l'efecte de comentari.

--- trunk/Code/Engine/Graphics/include/AnimatedModelManager.h
+++ trunk/Code/Engine/Graphics/include/AnimatedModelManager.h
@@ -14,22 +14,76 @@
 class CAnimatedInstanceModel;
 // ----------------------------------
 
+/**
+ * Classe AnimatedModelManager.
+ * Controlador de classes del tipus AnimatedCoreModel. S'encarrega de gestionar-les i fer la crida als seus render.
+**/
 class CAnimatedModelManager:
   public CMapManager<CAnimatedCoreModel>
 {
 public:
+  /**
+   * Constructor per defecte.
+   * Amb el constructor per defecte és suficient per deixar la classe en Ok.
+  **/
   CAnimatedModelManager(void)   {SetOk(true);};
+  /**
+   * Destructor.
+   * Allibera els recursos abans de destruir-se.
+  **/
   ~CAnimatedModelManager(void)  {Done();};
+  /**
+   * Mètode de càrrega desde fitxer.
+   * Aquest mètode inicialitzarà tots els AnimatedModel que es trobin al fitxer especificat.
+   * @param _szFileName Fitxer que conté els path d'AnimatedModel a carregar.
+   * @return True si s'ha iniciat correctament. False si no.
+  **/
   bool Load(const string &_szFileName);
+  /**
+   * Mètode de càrrega desde varis fitxers.
+   * Aquest mètode inicialitzarà tots els AnimatedModel que es trobin a cada un dels fitxers especificats.
+   * @param _XMLs Vector que conté els diferents fitxers des d'on carregar AnimatedModels.
+   * @return True si s'ha iniciat correctament. False si no.
+  **/
   bool Load(const vector<string>& _XMLs);
+  /**
+   * Mètode de recàrrega del Manager.
+   * Aquest mètode tornarà a inicialitzar tots els AnimatedModel que es trobin als fitxers especificats anteriorment.
+   * @return True si s'ha iniciat correctament. False si no.
+   * @see Load(const string &_szFileName)
+   * @see Load(const vector<string>& _XMLs)
+  **/
   bool Reload();
 
-  CAnimatedCoreModel*     GetCore(const std::string &Name);
-  CAnimatedInstanceModel* GetInstance(const std::string &Name);
+  /**
+   * Mètode per obtenir un AnimatedCoreModel.
+   * Aquest mètode retornarà l'AnimatedCoreModel especificat si és que aquest ha estat carregat anteriorment.
+   * @param _szName Identificador de l'AnimatedCoreModel a buscar.
+   * @return L'AnimatedCoreModel si existeix, NULL sino.
+  **/
+  CAnimatedCoreModel*     GetCore(const std::string& _szName);
+  /**
+   * Mètode d'obtenció d'AnimatedInstanceModel.
+   * Aquest mètode cercarà l'AnimatedCoreModel del nom especificat, i en cas d'existir en retornarà un AnimatedInstanceModel d'aquest.
+   * @param _szName Identificador de l'AnimatedCoreModel a buscar.
+   * @return L'AnimatedInstanceModel si existeix, NULL sino.
+  **/
+  CAnimatedInstanceModel* GetInstance(const std::string& _szName);
   
 private:
-  CAnimatedCoreModel*     GetCore(const std::string &Name, const std::string &Path);
+  /**
+   * Mètode per obtenir un AnimatedCoreModel.
+   * Aquest mètode retornarà l'AnimatedCoreModel especificat si és que aquest ha estat carregat anteriorment. Si no estava carregat el crearà i carregarà segons el fitxer especificat.
+   * @param _szName Identificador de l'AnimatedCoreModel a carregar.
+   * @param _szPath Fitxer d'on carregar l'AnimatedCoreModel en cas de no estar carregat amb anterioritat.
+   * @return L'AnimatedCoreModel.
+  **/
+  CAnimatedCoreModel*     GetCore(const std::string& _szName, const std::string& _szPath);
 
+  /**
+   * Conjunt de fitxers que indiquen on carregar AnimatedCoreModel.
+   * Cada fitxer nou que s'especifiqui mitjançant el mètode de Load s'adjuntarà a la llista. D'aquesta forma ReLoad sabrà quins fitxers recarregar.
+  **/
   set<string> m_vXMLFiles;
 };
 
