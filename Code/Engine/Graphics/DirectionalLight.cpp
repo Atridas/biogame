0e78b35ae6a462a39e418fde48cb9ca297321832
atridas87 <atridas87@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-01-11 17:49:03 +0000

    Ara les llums molen.

--- Code/Engine/Graphics/DirectionalLight.cpp
+++ Code/Engine/Graphics/DirectionalLight.cpp
@@ -8,20 +8,21 @@ void CDirectionalLight::Render(CRenderManager *_pRM) const
     Mat44f t, r, r2, r3, total;
     
     t.SetIdentity();
-    r.SetIdentity();
-    r2.SetIdentity();
-    r3.SetIdentity();
+    //r.SetIdentity();
+    //r2.SetIdentity();
+    //r3.SetIdentity();
 
     t.Translate(m_vPosition);
-    r.SetFromAngleY(-m_fYaw);
-    r2.SetFromAngleZ(m_fPitch);
-    r3.SetFromAngleX(m_fRoll);
+    //r.SetFromAngleY(-m_fYaw);
+    //r2.SetFromAngleZ(m_fPitch);
+    //r3.SetFromAngleX(m_fRoll);
 
-    total = t*r*r2*r3;
+    //total = t*r*r2*r3;
 
-    _pRM->SetTransform(total);
+    _pRM->SetTransform(t);
 
     _pRM->DrawAxis();
+    _pRM->DrawLine(v3fZERO,m_vDirection*2,colWHITE);
 }
 
 
