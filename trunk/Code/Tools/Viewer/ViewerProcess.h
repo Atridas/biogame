e6a82f0578406d837948f61c805a590c1516ff5a
galindix <galindix@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-02-06 19:21:32 +0000

    - Arxius del Viewer modificats pujats, en el proxim commit afegiré totes les DADES que falten degut a problemes amb el SVN.

---
+++ trunk/Code/Tools/Viewer/ViewerProcess.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include "Process.h"
+#include <base.h>
+
+//Forward declarations---------------------
+class CObject3D;
+class CRenderManager;
+//-----------------------------------------
+
+class CViewerProcess:
+  public CProcess
+{
+
+public:
+                        CViewerProcess            (): CProcess("Viewer Process"), m_pObject(0), m_pObjectCamera(0),
+                                                    m_fVelocity(0.0f),m_bRenderLights(false)
+                                                {};
+  virtual               ~CViewerProcess           ()                          {Done();};
+
+  virtual bool          Init();
+
+	void                  Update                  (float elapsedTime);
+	void					        DebugInformation        (float FPS);
+  void                  RenderScene             (CRenderManager* _pRM);
+
+  virtual bool          ExecuteProcessAction      (float _fDeltaSeconds, float _fDelta, const char* _pcAction);
+
+private:
+
+	void                  Release                 ();
+
+  int                   m_iState;
+  bool                  m_bStateChanged;
+  CObject3D*            m_pObject;
+  CObject3D*            m_pObjectBot;
+  CCamera*              m_pObjectCamera;
+  float                 m_fVelocity;
+  bool                  m_bRenderLights;
+};
