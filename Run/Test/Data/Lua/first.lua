7058aa124e8690ee2786c029b675b2e6e686433d
mudarra <mudarra@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-02-07 20:08:26 +0000

    - Els fitxers de LUA s'executen quan es llegeixen. - Arreglats els logs d'error d'execució de fitxer i codi.

--- Run/Test/Data/Lua/first.lua
+++ Run/Test/Data/Lua/first.lua
@@ -1,14 +1,8 @@
-log("LUA:: Init test script ")
-
-for i = 1, 10 do
-  local l_vPos = Vect3f(math.random(-15,15), 0.0, math.random(-15,15))
-  local l_pStaticModel = get_core():get_renderable_objects_manager():add_static("braç","bracet_"..i)
-  l_pStaticModel:set_position(l_vPos)
+function creaBracet()
+  for i = 1, 10 do
+    local l_vPos = Vect3f(math.random(-15,15), 0.0, math.random(-15,15))
+    local l_pStaticModel = _core:get_renderable_objects_manager():add_static("braç","bracet_"..g_ibracetCount)
+    g_ibracetCount = g_ibracetCount + 1
+    l_pStaticModel:set_position(l_vPos)
+  end
 end
-
---[[
-local pos = vect3f 0 0 0
-
-get_core():get_light_manager():get_resource("Spot01"):set_position(pos)
-
-]]
