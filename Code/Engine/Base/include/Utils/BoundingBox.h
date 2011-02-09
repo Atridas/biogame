e27880ce80e2884a15c7ee5fd45e3d8642f75fb4
mudarra <mudarra@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-02-09 17:03:39 +0000

    Inici de Col·lisions a BoundingBox, de moment no funcionen (no hi ha quasi re comprovat).

--- Code/Engine/Base/include/Utils/BoundingBox.h
+++ Code/Engine/Base/include/Utils/BoundingBox.h
@@ -10,17 +10,24 @@ class BoundingBox:
 public:
   BoundingBox();
   ~BoundingBox() { Done(); };
+
   bool Init(Vect3f _vMin, Vect3f _vMax);
   bool Init(char* _pVertexBuffer, unsigned short _usGeometryOffset, unsigned short _usVertexSize, unsigned short _usVertexCount);
 
-  Vect3f GetMin()  { return m_vBox[0]; };
-  Vect3f GetMax()  { return m_vBox[7]; };
+  bool Collides(BoundingBox _Target);
+
+  Vect3f GetMin() { return m_vBox[0]; };
+  Vect3f GetMax() { return m_vBox[7]; };
   Vect3f* GetBox() { return m_vBox; };
+  bool GetCollisional () { return m_bCollisional; };
+
+  void SetCollisional ( bool _bCollisional) { m_bCollisional = _bCollisional; };
 protected:
   virtual void Release();
   
 private:
   Vect3f m_vBox[8];
+  bool   m_bCollisional;
 };
 
 #endif
