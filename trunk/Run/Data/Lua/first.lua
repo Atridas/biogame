19c635477abe71ec68b0321e7fe695ec272f39fe
Atridas87 <Atridas87@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-02-12 14:21:59 +0000

    Canvi de directoris a Run.

--- trunk/Run/Data/Lua/first.lua
+++ trunk/Run/Data/Lua/first.lua
@@ -1,16 +1,8 @@
-
-local a = 2
-local b = 5
-
-
-local c = suma(a, b)
-
-log("LUA:: el valor es " .. c)
-
-
---[[
-local pos = vect3f 0 0 0
-
-get_core():get_light_manager():get_resource("Spot01"):set_position(pos)
-
-]]
+function creaBracet()
+  for i = 1, 10 do
+    local l_vPos = Vect3f(math.random(-15,15), 0.0, math.random(-15,15))
+    local l_pStaticModel = _core:get_renderable_objects_manager():add_static("braç","bracet_"..g_ibracetCount)
+    g_ibracetCount = g_ibracetCount + 1
+    l_pStaticModel:set_position(l_vPos)
+  end
+end
--- trunk/Run/Test/Data/Lua/first.lua
+++
@@ -1,8 +0,0 @@
-function creaBracet()
-  for i = 1, 10 do
-    local l_vPos = Vect3f(math.random(-15,15), 0.0, math.random(-15,15))
-    local l_pStaticModel = _core:get_renderable_objects_manager():add_static("braç","bracet_"..g_ibracetCount)
-    g_ibracetCount = g_ibracetCount + 1
-    l_pStaticModel:set_position(l_vPos)
-  end
-end
