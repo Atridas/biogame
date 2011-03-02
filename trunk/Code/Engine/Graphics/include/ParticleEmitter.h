004d0dd24445b9b48502b8f8ec7c7d487c1e03db
edual1985 <edual1985@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-03-02 20:33:23 +0000

    Semi acabada la Classe del emissor de particulas

--- trunk/Code/Engine/Graphics/include/ParticleEmitter.h
+++ trunk/Code/Engine/Graphics/include/ParticleEmitter.h
@@ -8,28 +8,50 @@
 #include <d3dx9.h>
 #include "Utils\RecyclingArray.h"
 #include "Particle.h"
+#include "RenderManager.h"
 
 class CParticleEmitter
 {
 public:
-  CParticleEmitter(void);
+  CParticleEmitter();
   ~CParticleEmitter(void);
 
   //  Set
-  void SetPosition(D3DXVECTOR3 _vPos){m_vPos=_vPos;};
-  void SetMinEmitRate(float _fMinEmitRate){m_fMinEmitRate = _fMinEmitRate;};
-  void SetMaxEmitRate(float _fMaxEmitRate){m_fMaxEmitRate = _fMaxEmitRate;};
-  void SetColor1(D3DXCOLOR _Color1){m_Color1 = _Color1;};
-  void SetColor2(D3DXCOLOR _Color2){m_Color2 = _Color2;};
-  void SetMinSize(float _fMinSize){m_fMinSize = _fMinSize;};
-  void SetMaxSize(float _fMaxSize){m_fMaxSize = _fMaxSize;};
-  void SetSpawnDir1(D3DXVECTOR3 _vSpawnDir1){m_vSpawnDir1 = _vSpawnDir1;};
-  void SetSpawnDir2(D3DXVECTOR3 _vSpawnDir2){m_vSpawnDir2 = _vSpawnDir2;};
+  void SetPosition          (D3DXVECTOR3& _vPos)                    {m_vPos=_vPos;};
+  void SetMinEmitRate       (float _fMinEmitRate)                   {m_fMinEmitRate = _fMinEmitRate;};
+  void SetMaxEmitRate       (float _fMaxEmitRate)                   {m_fMaxEmitRate = _fMaxEmitRate;};
+  void SetColor1            (D3DXCOLOR& _Color1)                    {m_Color1 = _Color1;};
+  void SetColor2            (D3DXCOLOR& _Color2)                    {m_Color2 = _Color2;};
+  void SetMinSize           (float _fMinSize)                       {m_fMinSize = _fMinSize;};
+  void SetMaxSize           (float _fMaxSize)                       {m_fMaxSize = _fMaxSize;};
+  void SetSpawnDir1         (D3DXVECTOR3& _vSpawnDir1)              {m_vSpawnDir1 = _vSpawnDir1;};
+  void SetSpawnDir2         (D3DXVECTOR3& _vSpawnDir2)              {m_vSpawnDir2 = _vSpawnDir2;};
+  void SetNumNewPartsExcess (float _fNumNewPartsExcess)             {m_fNumNewPartsExcess = _fNumNewPartsExcess;};
+  void SetTexture           (string& _sTexture)                     {m_sTexture = _sTexture;};
+  void SetDevice            (LPDIRECT3DDEVICE9 _pd3dDevice)         {m_pd3dDevice = _pd3dDevice;};
+  void SetParticle          (LPDIRECT3DVERTEXBUFFER9 _vbParticles)  {m_vbParticles = _vbParticles;};
 
 
   //Get
-  D3DXVECTOR3 GetPosition() const {return m_vPos;};
+  const D3DXVECTOR3&      GetPosition         () const {return m_vPos;};
+  float                   GetMinEmitRate      () const {return m_fMinEmitRate;};
+  float                   GetMaxEmitRate      () const {return m_fMaxEmitRate;};
+  const D3DXCOLOR&        GetColor1           () const {return m_Color1;};
+  const D3DXCOLOR&        GetColor2           () const {return m_Color2;};
+  float                   GetMinSize          () const {return m_fMinSize;};
+  float                   GetMaxSize          () const {return m_fMaxSize;};
+  const D3DXVECTOR3&      GetSpawnDir1        () const {return m_vSpawnDir1;};
+  const D3DXVECTOR3&      GetSpawnDir2        () const {return m_vSpawnDir2;};
+  float                   GetNumNewPartsExcess() const {return m_fNumNewPartsExcess;};
+  const string&           GetTexture          () const {return m_sTexture;};
+  LPDIRECT3DDEVICE9       GetDevice           () const {return m_pd3dDevice;};
+  LPDIRECT3DVERTEXBUFFER9 GetParticle         () const {return m_vbParticles;};
 
+
+  void                    Update              (float fElapsedTime);
+  void                    Init                (CRenderManager* rm, const string& _texureFileName);
+  void                    Release             ();
+  void                    Render              ();
 
 private:
 
@@ -46,6 +68,6 @@ private:
   string                                    m_sTexture;
   LPDIRECT3DDEVICE9                         m_pd3dDevice;
   LPDIRECT3DVERTEXBUFFER9                   m_vbParticles;
-  //CRecyclingArray<CParticle, NUMPARTICLES>  m_Particles;
+  CRecyclingArray<CParticle>                m_Particles;
 };
 #endif
