c010e28df23b079f8a80c7f62c8920135c7a778f
galindix <galindix@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-05-16 19:18:01 +0000

    Ragdolls: - Afegida la classe CPhysxRagdoll. - Amb el boto dret al proces de PhysX es crida al Init del Ragdoll - De moment carrega un XML amb la info dels actors relacionats amb els Bones - Funciona OK. pero falta molt

---
+++ trunk/Code/Engine/PhysX/include/PhysxRagdoll.h
@@ -0,0 +1,64 @@
+#pragma once
+#ifndef INC_PHYSIC_RAGDOLL_H
+#define INC_PHYSIC_RAGDOLL_H
+
+#include "Math/Color.h"
+#include "Math/MathUtils.h"
+#include "Math/Vector3.h"
+#include "Named.h"
+#include <vector>
+#include <cal3d/cal3d.h>
+
+//FORWARD DECLARATIONS/////////
+class NxPhysicsSDK;
+class NxScene;
+class NxControllerManager;
+class CPhysicUserAllocator;
+class CPhysicCookingMesh;
+class CPhysicActor;
+class NxActor;
+class NxCCDSkeleton;
+class CPhysicController;
+class CRenderManager;
+class CPhysicSphericalJoint;
+class CPhysicRevoluteJoint;
+class CPhysicFixedJoint;
+class CPhysicTriggerReport;
+class CScriptManager;
+class CPhysicUserData;
+///////////////////////////////
+struct SBoneActor {
+ string m_pActorName;
+ string m_szBoneName;
+ string m_szType;
+ Vect3f m_vSize;
+ float  m_Density;
+ CPhysicActor* m_pActor;
+ CPhysicUserData* m_pUserData;
+};
+
+class CPhysxRagdoll:
+  public CNamed,
+  public CBaseControl
+{
+public:
+  //Constructors
+  CPhysxRagdoll(string _szName) : CNamed(_szName),m_pSkeleton(0) {};
+  ~CPhysxRagdoll(){Done();Release();};
+
+  //Funcions
+  bool						Load	(const string& _szFileName, bool _bReload);
+  bool						Init	(CalSkeleton* _pSkeleton);
+
+  //CGameObject(string _szName) : m_pRenderableObject(0), m_pPhysXActor(0),CNamed(_szName),m_pUserData(0),m_szType("") {};
+protected:
+  void                    Release();   
+  
+
+private:
+  vector<SBoneActor> m_vBoneActors;
+  CalSkeleton* m_pSkeleton;
+};
+
+
+#endif //INC_PHYSIC_CONTROLLER_H
