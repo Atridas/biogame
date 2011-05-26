f8a2e524d7813f13b56448ebe5fd8e5ea80e3d21
galindix <galindix@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-05-26 19:16:25 +0000

    Canvis a les classes Skeleton i Bone del Ragdoll nostre. Preparat per liarla parda.

--- Code/Engine/PhysX/include/PhysxBone.h
+++ Code/Engine/PhysX/include/PhysxBone.h
@@ -5,21 +5,41 @@
 #include "base.h"
 
 class CalBone;
+class CPhysicActor;
+class CPhysicUserData;
 
 class CPhysxBone : CBaseControl
 {
 public:
-  CPhysxBone() : m_pCalBone(0) {};
+  CPhysxBone() : m_pCalBone(0), m_iParentID(-1), m_pActor(0), m_bRoot(true),m_pBoneUserData(0) {};
   ~CPhysxBone() {Done();};
 
-  bool Init();
-  virtual void Release();
+  bool                Init(CalBone* _pBone);
+  virtual void        Release();
 
-  void UpdateCal3dFromPhysx();
+  void                UpdateCal3dFromPhysx();
+  CalBone*            GetCalBone()                           {return m_pCalBone;};
+  void                SetCalBone(CalBone* _pCalBone)         {m_pCalBone = _pCalBone;};
+  bool                IsBoneRoot()                           {return m_bRoot;};
 
+  //Funcions per inicialitzar la info del Bone (primer matrius, segon actors, tercer joints)
+  void                InitBoneMatrix();
+  void                InitPhysXActor();
+  void                InitPhysXJoint();
+
+
+  
 private:
 
-  CalBone* m_pCalBone;
+  CalBone*            m_pCalBone;
+  int                 m_iParentID;
+  bool                m_bRoot;
+  vector<int>         m_vChildListID;
+
+  Mat44f              m_vMatActor;
+
+  CPhysicActor*       m_pActor;
+  CPhysicUserData*    m_pBoneUserData;
 };
 
 #endif
