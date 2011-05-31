78ac240849dd63fa4137c8161f64b76c2f53f125
galindix <galindix@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-05-31 19:22:31 +0000

    Més funcionalitats de Ragdoll ja posant JOINTS esferics. Encara falta.

--- Code/Engine/PhysX/PhysicSphericalJoint.cpp
+++ Code/Engine/PhysX/PhysicSphericalJoint.cpp
@@ -35,6 +35,7 @@ void CPhysicSphericalJoint::SetInfo	(const Vect3f& anchor, CPhysicActor* actorA,
 	}
 
 	NxVec3 pos(anchor.x, anchor.y, anchor.z );
+
 	m_pSphericalDesc->setToDefault();
 	m_pSphericalDesc->actor[0] = actorA->GetPhXActor();
 	if (actorB!=NULL)
@@ -46,5 +47,44 @@ void CPhysicSphericalJoint::SetInfo	(const Vect3f& anchor, CPhysicActor* actorA,
 		m_pSphericalDesc->actor[1] = NULL;	
 	}
 
+
 	m_pSphericalDesc->setGlobalAnchor(pos);
+
+   
+ 
+}
+
+void CPhysicSphericalJoint::SetInfoComplete	(const Vect3f& anchor, const Vect3f& _axis, CPhysicActor* actorA,  CPhysicActor* actorB)
+{
+	if (actorA==NULL)
+	{
+		LOGGER->AddNewLog(ELL_ERROR, "PhysicSphericalJoint:: El primer actor pasado como argumento no puede ser null");
+		return;
+	}
+
+	NxVec3 pos(anchor.x, anchor.y, anchor.z );
+  NxVec3 axis(_axis.x, _axis.y, _axis.z );
+
+	m_pSphericalDesc->setToDefault();
+	m_pSphericalDesc->actor[0] = actorA->GetPhXActor();
+	if (actorB!=NULL)
+	{
+		m_pSphericalDesc->actor[1] = actorB->GetPhXActor();	
+	}
+	else
+	{
+		m_pSphericalDesc->actor[1] = NULL;	
+	}
+
+  m_pSphericalDesc->flags |= NX_SJF_SWING_LIMIT_ENABLED;
+  m_pSphericalDesc->swingLimit.value = 0.3*NxPi;
+
+  m_pSphericalDesc->flags |= NX_SJF_TWIST_LIMIT_ENABLED;
+  m_pSphericalDesc->twistLimit.low.value = -0.05*NxPi;
+  m_pSphericalDesc->twistLimit.high.value = 0.05*NxPi;
+
+	m_pSphericalDesc->setGlobalAnchor(pos);
+  m_pSphericalDesc->setGlobalAxis(axis);
+   
+ 
 }
