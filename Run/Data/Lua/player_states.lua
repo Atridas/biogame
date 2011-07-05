1dce566c78cf30cf195fe0b7f5e341a4dacba617
sergivalls <sergivalls@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-07-05 21:49:06 +0000

    Cobertura baixa + Aim + Shoot. Les transisions es fan amb blend sense animacio, pq sino les transisions son lentes, brusques i ximples. Aixo fa que fagi ruletes rollo Michael Jackson, que quadra força amb l'estetica metrosexual del Riggle.

--- Run/Data/Lua/player_states.lua
+++ Run/Data/Lua/player_states.lua
@@ -18,6 +18,7 @@ State_Player_Cobertura_Baixa = {}
 State_Player_Cobertura_Baixa_Sortir = {}
 State_Player_Cobertura_Alta = {}
 State_Player_Cobertura_Alta_Sortir = {}
+State_Player_Cobertura_Baixa_Apuntar = {}
 
 -------------------------------------------------------------------------------------------------
 camera_player = function(_jugador, _dt)
@@ -190,8 +191,8 @@ State_Player_Apuntar['Enter'] = function(_jugador)
   local animation = _jugador:get_component(BaseComponent.animation)
   local mirilla = _jugador:get_component(BaseComponent.mirilla)
   
-  animation:clear_all_cycles(0.0)
-  animation:play_cycle('aim', 0.3)
+  animation:clear_all_cycles(0.1)
+  animation:play_cycle('aim', 0.1)
 	mirilla:set_active(true)
 	
   --end
@@ -202,11 +203,11 @@ end
 State_Player_Apuntar['Exit'] = function(_jugador)
   --log('exit player apuntant')
   
-  local player_controller = _jugador:get_component(BaseComponent.player_controller)
-  local animation = _jugador:get_component(BaseComponent.animation)
+  --local player_controller = _jugador:get_component(BaseComponent.player_controller)
+  --local animation = _jugador:get_component(BaseComponent.animation)
   local mirilla = _jugador:get_component(BaseComponent.mirilla)
    
-  animation:clear_cycle('aim',0.3)
+  --animation:clear_cycle('aim',0.3)
   mirilla:set_active(false)
 
 end
@@ -482,8 +483,8 @@ State_Player_Cobertura_Baixa['Enter'] = function(_jugador)
   renderable_object:set_yaw(-((player_controller.cover_normal):get_angle_y()) + math.pi)
   
   --Amb animacio
-  --animation:clear_all_cycles(0.0)
-  --animation:play_cycle('CoverAvallDretaIdle', 1.0)
+  --animation:clear_all_cycles(0.3)
+  --animation:play_cycle('CoverAvallDretaIdle', 0.3)
   --animation:play('CoverAvallDreta', 0.3, 1.0, false)
   
   --Nomes blend
@@ -510,6 +511,10 @@ State_Player_Cobertura_Baixa['Update'] = function(_jugador, _dt)
     _jugador:get_component(BaseComponent.state_machine):get_state_machine():change_state('State_Player_Cobertura_Baixa_Sortir')
   end
 
+  if ACTION_MANAGER:is_action_active('Aim') then
+    _jugador:get_component(BaseComponent.state_machine):get_state_machine():change_state('State_Player_Cobertura_Baixa_Apuntar')
+    return
+  end
 end
 
 -------------------------------------------------------------------------------------------------
@@ -581,6 +586,8 @@ end
 -------------------------------------------------------------------------------------------------
 State_Player_Cobertura_Alta['Enter'] = function(_jugador)
   
+  log('Michael Jackson YEAH!')
+  
   local player_controller = _jugador:get_component(BaseComponent.player_controller)
   local animation = _jugador:get_component(BaseComponent.animation)
   local moviment = _jugador:get_component(BaseComponent.movement)
@@ -593,11 +600,17 @@ State_Player_Cobertura_Alta['Enter'] = function(_jugador)
   
   renderable_object:set_yaw(-((player_controller.cover_normal):get_angle_y()) + math.pi)
   
-  animation:clear_all_cycles(0.0)
-  animation:play_cycle('CoverDretaIdle', 1.0)
-  animation:play('CoverDreta', 0.3, 1.0, false)
-  player_controller.time = 0
+  --Amb animacio
+  --animation:clear_all_cycles(0.3)
+  --animation:play_cycle('CoverDretaIdle', 1.0)
+  --animation:play('CoverDreta', 0.3, 1.0, false)
   
+  
+  --Nomes blend
+  animation:clear_all_cycles(0.3)
+  animation:play_cycle('CoverDretaIdle', 0.3)
+  
+  player_controller.time = 0
   renderable_object.block_yaw = true
 end
 
@@ -635,10 +648,18 @@ end
 -------------------------------------------------------------------------------------------------
 State_Player_Cobertura_Alta_Sortir['Enter'] = function(_jugador)
   
+  log('Michael Jackson YEAH!')
+  
   local player_controller = _jugador:get_component(BaseComponent.player_controller)
   local animation = _jugador:get_component(BaseComponent.animation)
+  
+  --Amb animacio
+  --animation:clear_all_cycles(0.3)
+  --animation:play('CoverSortidaDreta',0.3,1.0,false)
+  
+  --Nomes blend
   animation:clear_all_cycles(0.3)
-  animation:play('CoverSortidaDreta',0.3,1.0,false)
+  animation:play_cycle('idle',0.3)
   
   _jugador:get_component(BaseComponent.renderable_object).block_yaw = true
   player_controller.time = 0
@@ -659,7 +680,7 @@ State_Player_Cobertura_Alta_Sortir['Update'] = function(_jugador, _dt)
 
   player_controller.time = player_controller.time + _dt
   
-  if player_controller.time >= 0.45 then
+  if player_controller.time >= 0.3 then
     _jugador:get_component(BaseComponent.state_machine):get_state_machine():change_state('State_Player_Neutre')
   end
 
@@ -667,6 +688,81 @@ end
 
 -------------------------------------------------------------------------------------------------
 State_Player_Cobertura_Alta_Sortir['Receive'] = function(_jugador, _event)
+
+  if _event.msg == Event.rebre_impacte then
+    _jugador:get_component(BaseComponent.state_machine):get_state_machine():change_state('State_Player_Tocat')
+  elseif _event.msg == Event.morir then
+    _jugador:get_component(BaseComponent.state_machine):get_state_machine():change_state('State_Player_Morint')
+  end
+  
+end
+
+-------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------
+-- Cobertura_Baixa_Apuntar --------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------------
+State_Player_Cobertura_Baixa_Apuntar['Enter'] = function(_jugador)
+  
+  log('Michael Jackson YEAH!')
+  
+  local player_controller = _jugador:get_component(BaseComponent.player_controller)
+  local animation = _jugador:get_component(BaseComponent.animation)
+  local mirilla = _jugador:get_component(BaseComponent.mirilla)
+  
+  --Nomes blend
+  animation:clear_all_cycles(0.2)
+  animation:play_cycle('aim', 0.2)
+  
+  --Amb animacio
+  --animation:clear_all_cycles(0.3)
+  --animation:play_cycle('aim', 0.3)
+  --animation:play('CoverSortidaAvallDreta', 0.3, 1.0, false)
+  
+	mirilla:set_active(true)
+  
+  _jugador:get_component(BaseComponent.renderable_object).block_yaw = false
+  player_controller.time = 0
+  
+end
+
+-------------------------------------------------------------------------------------------------
+State_Player_Cobertura_Baixa_Apuntar['Exit'] = function(_jugador)
+
+  log('Michael Jackson YEAH!')
+  --_jugador:get_component(BaseComponent.renderable_object).block_yaw = false
+  
+end
+
+-------------------------------------------------------------------------------------------------
+State_Player_Cobertura_Baixa_Apuntar['Update'] = function(_jugador, _dt)
+
+  local pitch, yaw, object3d = camera_player(_jugador, _dt)
+  local player_controller = _jugador:get_component(BaseComponent.player_controller)
+  local animation = _jugador:get_component(BaseComponent.animation)
+  
+  if not ACTION_MANAGER:is_action_active('Aim') then
+    _jugador:get_component(BaseComponent.state_machine):get_state_machine():revert_state()
+  end
+  
+  if ACTION_MANAGER:is_action_active('Shoot') then
+    animation:play('shoot', 0.3, 1.0, false)
+    player_controller:shoot()
+    SOUND:play_sample('disparar')
+  end
+  
+  --local player_controller = _jugador:get_component(BaseComponent.player_controller)
+
+  --player_controller.time = player_controller.time + _dt
+  
+  --if player_controller.time >= 0.45 then
+  --  _jugador:get_component(BaseComponent.state_machine):get_state_machine():change_state('State_Player_Neutre')
+  --end
+
+end
+
+-------------------------------------------------------------------------------------------------
+State_Player_Cobertura_Baixa_Apuntar['Receive'] = function(_jugador, _event)
 
   if _event.msg == Event.rebre_impacte then
     _jugador:get_component(BaseComponent.state_machine):get_state_machine():change_state('State_Player_Tocat')
