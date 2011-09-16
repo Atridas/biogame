d7f5f04455c1fe77dfb33409029c6f61d07b9c32
Atridas87 <Atridas87@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-09-16 15:11:18 +0000

    Reformulats els xml de portals per que tinguin un nom més adecuat a la funció que fan; creat un fitxer que permet iniciar el biogame en el nivell que vulguis.

---
+++ Code/Game/BioGame/include/NivellInicial.h
@@ -0,0 +1,48 @@
+#pragma once
+#ifndef __NIVELL_INICIAL__
+#define __NIVELL_INICIAL__
+
+//#define BIOGAME_NIVELLS_DEFAULT
+//#define BIOGAME_NIVELLS_NIVELL_MENYS_2
+//#define BIOGAME_NIVELLS_NIVELL_MENYS_1
+#define BIOGAME_NIVELLS_HANGAR
+
+
+
+#ifdef BIOGAME_NIVELLS_DEFAULT
+#ifdef BIOGAME_NIVELLS_NIVELL_MENYS_2
+#error 2 nivell definits a la vegada!
+#endif
+#ifdef BIOGAME_NIVELLS_NIVELL_MENYS_1
+#error 2 nivell definits a la vegada!
+#endif
+#ifdef BIOGAME_NIVELLS_HANGAR
+#error 2 nivell definits a la vegada!
+#endif
+#endif
+
+#ifdef BIOGAME_NIVELLS_NIVELL_MENYS_2
+#ifdef BIOGAME_NIVELLS_NIVELL_MENYS_1
+#error 2 nivell definits a la vegada!
+#endif
+#ifdef BIOGAME_NIVELLS_HANGAR
+#error 2 nivell definits a la vegada!
+#endif
+#endif
+
+#ifdef BIOGAME_NIVELLS_NIVELL_MENYS_1
+#ifdef BIOGAME_NIVELLS_HANGAR
+#error 2 nivell definits a la vegada!
+#endif
+#endif
+
+#ifndef BIOGAME_NIVELLS_DEFAULT
+#ifndef BIOGAME_NIVELLS_NIVELL_MENYS_2
+#ifndef BIOGAME_NIVELLS_NIVELL_MENYS_1
+#ifndef BIOGAME_NIVELLS_HANGAR
+#error No hi ha cap nivell definit
+#endif
+#endif
+#endif
+#endif
+#endif
