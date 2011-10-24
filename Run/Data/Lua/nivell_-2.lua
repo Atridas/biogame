9b7b854e3ad9828ce29e79fe31a7835d35aef794
mudarra <mudarra@576ee6d0-068d-96d9-bff2-16229cd70485>
2011-10-24 16:17:05 +0000

    Nova lògica del -1.

--- Run/Data/Lua/nivell_-2.lua
+++ Run/Data/Lua/nivell_-2.lua
@@ -60,21 +60,6 @@ function laboratori_obrir_porta(_self)
     
     --Mostrar tutorial d'objectes destructibles
     tutorial_barrel()
-    
-    --Apagar llums
-    --local l_light = LM:get_resource("lvl2_lab_Spot05")
-    --if l_light then
-    --  l_light:set_active(false)
-    --else
-    --  log("No es troba lvl2_lab_Spot05")
-    --end
-    --
-    --local l_light = LM:get_resource("lvl2_lab_Omni03")
-    --if l_light then
-    --  l_light:set_active(false)
-    --else
-    --  log("No es troba lvl2_lab_Omni03")
-    --end
   else
     log('error, no es troba la porta')
   end
